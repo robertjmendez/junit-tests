c0109e4e30b99f500e46ab19eafcab95daaa8081
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-07-07 16:00:19 -0500

    Created the Student class and added tests to the StudentTest class

---
+++ src/main/java/Student.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private long id;
+    private ArrayList<Integer> grades;
+
+    public Student(String name, long id) {
+        this.name = name;
+        this.id = id;
+        this.grades = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addGrade(int grade) {
+        grades.add(grade);
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public double getGradeAverage() {
+        int sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return sum / (double) grades.size();
+    }
+}
+
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
 public class StudentTest {
+    private Student student;
+
+    @Before
+    public void setUp(){
+        student = new Student("John Doe", 1);
+    }
+
+    @Test
+    public void testStudentConstructorSetsNameAndId() {
+        assertEquals("John Doe", student.getName());
+        assertEquals(1, student.getId());
+    }
+
+    @Test
+    public void testStudentStartsWithNoGrades() {
+        assertEquals(new ArrayList<Integer>(), student.getGrades());
+    }
+
+    @Test
+    public void testAddGradeChangesGrades() {
+        student.addGrade(90);
+        ArrayList<Integer> expectedGrades = new ArrayList<>();
+        expectedGrades.add(90);
+        assertEquals(expectedGrades, student.getGrades());
+    }
+
+    @Test
+    public void testGetGradeAverageCalculatesCorrectAverage() {
+        student.addGrade(100);
+        student.addGrade(75);
+        assertEquals(87.5, student.getGradeAverage(), 0.001);
+    }
 }
